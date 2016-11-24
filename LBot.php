e21b873aea94d50213dd81bb6719e36310de20e0
Bireley <b_bireley@hotmail.com>
2016-11-24 11:25:52 +0700

    Update LBot.php

--- LBot.php
+++ LBot.php
@@ -8,51 +8,35 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
-	file_put_contents("php://stderr", "hello, this is a test : "  . $content  . "  \n");
+	//file_put_contents("php://stderr", "hello, this is a test : "  . $content  . "  \n");
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = "B : " . $event['message']['text'];
-			// Get replyToken
-			
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
 
-			
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages]
-			];
-		
 		}
 		else 
 		{
-		
-			$text = "You have send message type : " . $event['message']['type'] . " to me.";
-			// Get replyToken
-			
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages]
-			];
-		
+			$text = "You have send message type : " . $event['message']['type'] . " to me.";	
 				
 		}
 	
 		// Make a POST Request to Messaging API to reply to sender
 		$url = 'https://api.line.me/v2/bot/message/reply';
-		// $replyToken = $event['replyToken'];
+		$replyToken = $event['replyToken'];
+		// Build message to reply back
+		$messages = [
+			'type' => 'text',
+			'text' => $text
+		];
+
+
+		$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages]
+		];
+		
 		$post = json_encode($data);
 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 		
