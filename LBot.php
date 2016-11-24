e153c83fe4b6c43df9755b9ed7d9cd9d2d483d4f
Bireley <b_bireley@hotmail.com>
2016-11-24 11:02:06 +0700

    Update LBot.php

--- LBot.php
+++ LBot.php
@@ -8,6 +8,7 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
+	file_put_contents("php://stderr", "hello, this is a test : "  . $event['message']['type']  . "  \n");
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
